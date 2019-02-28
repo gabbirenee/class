b1f24b4574d59892e553610f171c078d0da850a3
Gabbi Forsythe <gforsythe17@gmail.com>
2019-02-28 14:17:35 -0500

    Added header, footer, nav elements on all the hink pages

--- thanksHink.php
+++ thanksHink.php
@@ -4,20 +4,28 @@
         <title>Hink Contact Us</title>
     </head>
     <body>
-        <a href="hinkHome.html"><img src="images/ButlerLogo.png" alt="Butler University" width="190" height="90" /></a>
-        <ul>
-            <li>Resources</li>
-            <li>Information</li>
-            <li><a href="hinkForm.html">Form</a></li>
-            <li><a href="hinkContact.html">Contact Us</a></li>
-        </ul>
-        <img src="images/ButlerBulldogMascot.jpg" height="413" width="488" alt="picture of Hink Mascot" title="Hink wants YOU to tryout" />
-        <h1>Contact Hink's Team</h1>
-        <p>Thank you for applying for the position. Someone will get back to you as soon as possible.</p>
-        <ul>
-            <li><a href="hinkHome.html">Home</a></li>
-            <li><a href="https://www.butler.edu/">Butler University</a></li>
-            <li><a href="hinkContact.html">Contact Us</a></li>
-        </ul>
+        <header>
+            <a href="hinkHome.html"><img src="images/ButlerLogo.png" alt="Butler University" width="190" height="90" /></a>
+            <nav>
+                <ul>
+                    <li>Resources</li>
+                    <li>Information</li>
+                    <li><a href="hinkForm.html">Form</a></li>
+                    <li>Contact Us</li>
+                </ul>
+            </nav>
+        </header>
+        <div id="hinkBox">
+            <img src="images/ButlerBulldogMascot.jpg" height="413" width="488" alt="picture of Hink Mascot" title="Hink wants YOU to tryout" />
+            <h1>Contact Hink's Team</h1>
+            <p>Thank you for applying for the position. Someone will get back to you as soon as possible.</p>
+        </div>
+        <footer>
+            <ul>
+                <li><a href="hinkHome.html">Home</a></li>
+                <li><a href="https://www.butler.edu/">Butler University</a></li>
+                <li><a href="hinkContact.html">Contact Us</a></li>
+            </ul>
+        </footer>
     </body>
 </html>
