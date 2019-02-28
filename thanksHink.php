f5b4eac676e36a11674883c607d29e8f1c803134
Gabbi Forsythe <gforsythe17@gmail.com>
2019-02-28 13:32:35 -0500

    added the thanksHink.php file to be displayed when the form submission is correct

---
+++ thanksHink.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hink Contact Us</title>
+    </head>
+    <body>
+        <a href="hinkHome.html"><img src="images/ButlerLogo.png" alt="Butler University" width="190" height="90" /></a>
+        <ul>
+            <li>Resources</li>
+            <li>Information</li>
+            <li><a href="hinkForm.html">Form</a></li>
+            <li><a href="hinkContact.html">Contact Us</a></li>
+        </ul>
+        <img src="images/ButlerBulldogMascot.jpg" height="413" width="488" alt="picture of Hink Mascot" title="Hink wants YOU to tryout" />
+        <h1>Contact Hink's Team</h1>
+        <p>Thank you for applying for the position. Someone will get back to you as soon as possible.</p>
+        <ul>
+            <li><a href="hinkHome.html">Home</a></li>
+            <li><a href="https://www.butler.edu/">Butler University</a></li>
+            <li><a href="hinkContact.html">Contact Us</a></li>
+        </ul>
+    </body>
+</html>
