6e4d54e3200863f06f4029a336fa8f858b12dbef
Gabbi Forsythe <gforsythe17@gmail.com>
2019-02-28 13:29:03 -0500

    Added all the form elements for hinkContact.html and added php files

---
+++ hinkForm.php
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>contact</title>
+</head>
+
+<body>
+
+<?php
+
+$firstName = $_POST["fName"];
+$lastName = $_POST["lName"];
+$email = $_POST["email"];
+$comment = $_POST["comments"];
+$subject = "Be HINK! Tryout Application";
+
+//Enter your email address
+$to = "gforsyth@butler.edu";
+
+
+//Prepare Email Body Text
+	$Body = "";
+	$Body .= "Name: ";
+	$Body .= $firstName . ' ' . $lastName;
+	$Body .= "\n";
+	$Body .= "Email: ";
+	$Body .= $email;
+	$Body .= "\n";
+	$Body .= "Comments: ";
+	$Body .= $comment;
+	$Body .= "\n";
+
+//Send Email
+$send_contact= mail($to,$subject,$Body);
+if($send_contact) {
+	print "<meta http-equiv=\"refresh\" content=\"0;URL=thanksHink.php\">";
+}
+?>
+
+
+
+</body>
+</html>
